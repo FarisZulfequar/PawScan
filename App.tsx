d19cc0e98764fc200289ceebf02c5258d8d34c6f
Anais-Z <anaiszed447@gmail.com>
2026-05-07 18:23:49 -0400

    Imported Login Screen

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import AuthScreen from "./src/screens/AuthScreen";
+import LoginScreen from "./src/screens/LoginScreen";
 
 export default function App() {
   return (
